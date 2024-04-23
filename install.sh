9b50c5e21607c3bf23622e1317c56996d74eaf28
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-04-23 18:50:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 curl https://dl.google.com/linux/linux_signing_key.pub \
-    | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/chrome-remote-desktop.gpg
+    | sudo gpg --dearmon -o /etc/apt/trusted.gpg.d/chrome-remote-desktop.gpg
 echo "deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main" \
     | sudo tee /etc/apt/sources.list.d/chrome-remote-desktop.list
 sudo apt-get update
