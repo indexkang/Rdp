7db4c787682df9e0d1e324009074476b50c7c555
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-04-23 19:31:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,13 @@ sudo DEBIAN_FRONTEND=noninteractive \
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
 sudo apt install --assume-yes /tmp/chrome-remote-desktop_current_amd64.deb
     
- $1 --pin=123456
  
 sudo DEBIAN_FRONTEND=noninteractive \
     sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
     
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo systemctl disable lightdm.service
-sudo apt install --assume-yes task-xfce-desktop
+sudo apt install --assume-yes xfce-desktop
 
 echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1600x1200,3840x2560" \
     >> ~/.profile
